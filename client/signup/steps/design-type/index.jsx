44b993ea44027218d893d2a2adf9e1e615e6bad5
kwight <kwight@kwight.ca>
2016-06-27 12:37:17 -0700

    Signup: Add a Store option to the design type step.

--- client/signup/steps/design-type/index.jsx
+++ client/signup/steps/design-type/index.jsx
@@ -19,6 +19,7 @@ export default React.createClass( {
 			{ type: 'blog', label: this.translate( 'A list of my latest posts' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 230"><rect x="15" y="15" fill="#E8F0F5" width="280" height="70"/><rect x="15" y="98" fill="#C3EF96" width="194" height="85"/><rect x="15" y="195" fill="#C3EF96" width="194" height="35"/></svg> },
 			{ type: 'page', label: this.translate( 'A welcome page for my site' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 230"><rect fill="#E8F0F5" width="310" height="110"/><rect x="114" y="205" fill="#E8F0F5" width="82" height="25"/><rect x="15" y="205" fill="#E8F0F5" width="82" height="25"/><rect x="213" y="205" fill="#E8F0F5" width="82" height="25"/><rect x="15" y="36" fill="#D2DEE6" width="153" height="13"/><rect x="15" y="59" fill="#D2DEE6" width="113" height="13"/><rect x="15" y="82" fill="#C3EF96" width="30" height="13"/><rect x="15" y="125" fill="#C3EF96" width="280" height="65"/></svg> },
 			{ type: 'grid', label: this.translate( 'A grid of my latest posts' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 230"><rect x="15" y="15" fill="#E8F0F5" width="280" height="40"/><rect x="114" y="70" fill="#C3EF96" width="82" height="65"/><rect x="15" y="70" fill="#C3EF96" width="82" height="65"/><rect x="213" y="70" fill="#C3EF96" width="82" height="65"/><rect x="114" y="150" fill="#C3EF96" width="82" height="65"/><rect x="15" y="150" fill="#C3EF96" width="82" height="65"/><rect x="213" y="150" fill="#C3EF96" width="82" height="65"/></svg> },
+			{ type: 'store', label: this.translate( 'An online store' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 309 231"><defs><path id="a" d="M0 0h309v231H0z"/><mask id="b" x="0" y="0" width="309" height="231" fill="#fff"><use xlinkHref="#a"/></mask></defs><g fill="none" fill-rule="evenodd"><use stroke="#DDE6ED" mask="url(#b)" stroke-width="2" fill="#FFF" xlinkHref="#a"/><path fill="#C4EF96" d="M15 122h82v65H15zM15 202h82v28H15zM211 122h82v65h-82zM211 202h82v28h-82zM113 122h82v65h-82zM113 202h82v28h-82z"/><path fill="#F3F6F8" d="M1 1h307v105H1z"/><path fill="#D2DEE6" d="M78 22h153v13H78zM98 45h113v13H98z"/><path fill="#C4EF96" d="M139 68h31v13h-31z"/></g></svg> },
 		];
 	},
 
@@ -37,7 +38,6 @@ export default React.createClass( {
 		return (
 			<div className="design-type__list">
 				{ this.getChoices().map( this.renderChoice ) }
-				<div className="design-type__choice is-spacergif" />
 			</div>
 		);
 	},
