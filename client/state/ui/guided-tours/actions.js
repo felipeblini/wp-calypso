59b80b6c73f1b10d6b89cbc7f1a4b209b69ebbd7
Miguel Fonseca <miguelcsf@gmail.com>
2016-06-27 13:36:36 +0100

    Explain issues w/ current impl of addSeenGuidedTour

--- client/state/ui/guided-tours/actions.js
+++ client/state/ui/guided-tours/actions.js
@@ -76,6 +76,12 @@ export function nextGuidedTourStep( { tour = 'main', stepName } ) {
 }
 
 // TODO(mcsf): come up with a much better (read: safer) solution
+//
+// The way we persist which tours have been seen by the user is subject to
+// concurrency issues and history loss, since adding a tour to that collection
+// is actually achieved by adding a tour to the client's copy of the collection
+// and saving that as the new history.
+
 function addSeenGuidedTour( getState, tourName, finished = false ) {
 	return savePreference( 'guided-tours-history', [
 		...getPreference( getState(), 'guided-tours-history' ),
